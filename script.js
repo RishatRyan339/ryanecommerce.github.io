4cfab72095a8d269b24f24ca0a709549cf12adac
Rishat Ryan <rishatryan4452@gmail.com>
2022-09-07 00:00:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+
+
+}
+
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+
+
+}
+
+// -------------------------------------------
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smalling[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+
+smalling[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+
+smalling[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+
+smalling[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
